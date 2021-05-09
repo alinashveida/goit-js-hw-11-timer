f74e4acd7bc173c2ab0f2ef7f97f79e76aee1d22
alinashveida <alinashveida@gmail.com>
2021-05-09 19:51:18 +0300

    1

---
+++ index.js
@@ -0,0 +1,49 @@
+// new CountdownTimer({
+//     selector: '#timer-1',
+//     targetDate: new Date('Jul 17, 2019'),
+//   });
+
+  const targetDate = new Date('Jul 17, 2019')
+//   console.log(CountdownTimer)
+   const time = Date.parse(targetDate) - Date.parse(new Date());
+
+   console.log(time)
+  /*
+ * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+ * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+ */
+const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+console.log(days)
+
+/*
+ * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+ * остатка % и делим его на количество миллисекунд в одном часе
+ * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+ */
+const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+console.log(hours)
+
+/*
+ * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+ * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+ */
+const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+console.log(mins)
+
+/*
+ * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+ * миллисекунд в одной секунде (1000)
+ */
+const secs = Math.floor((time % (1000 * 60)) / 1000);
+console.log(secs)
+
+// class Timer{
+
+// }
+
+function getTime(){
+
+}
+const date = new Date()
+console.log(date)
